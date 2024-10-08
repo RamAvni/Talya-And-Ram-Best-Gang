dfe1136d0fa846eb10f8e613f588e8dfab5fa658
TalyaAlbeck <talyaalbeck@hilma.tech>
2024-10-08 13:22:20 +0300

    with css in the form

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const container = document.getElementById("container");
 
 function showPage() {
     document.getElementById("defult").style.display = "none";
-    // document.getElementById("defult2").style.display = "none";
+    document.getElementById("defult2").style.display = "none";
     const template = document.getElementById(this.getAttribute("data-section"));
     container.innerHTML = ``
     container.appendChild(template.content);
