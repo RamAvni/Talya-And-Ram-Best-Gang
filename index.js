3c3b9b19742a332fcb552bf9e29efec093375d95
RamAvni <ramavni@hilma.tech>
2024-10-08 12:35:43 +0300

    ergerf

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+// Page Changing
+
 for (let link of document.querySelectorAll(".link")) {
   link.addEventListener("click", showPage);
 }
@@ -8,4 +11,14 @@ function showPage() {
   const template = document.getElementById(this.getAttribute("data-section"));
   container.innerHTML= ``
   container.appendChild(template.content);
+}
+
+
+
+// Game
+
+document.getElementById("game-mole").addEventListener("click", changeMolePlace)
+
+function changeMolePlace(){
+    this.
 }
